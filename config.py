60c51bf0403d8ae7856763873508d0b96cb8b291
julienroy13 <jul.roy1311@gmail.com>
2017-11-21 12:32:10 -0500

    First try to integrate everything. Code still won't run.

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 global config
 config = {}
 config['img_size'] = 256  # 64, 128, 256, 512
-config['patch_size'] = 3
-config['number_of_patches_per_zone'] = 1
-config['n_iter'] = 2
+
+config['patch_size'] = 3 # For PatchMatch
+config['number_of_patches_per_zone'] = 1 # For randomSearch in PatchMatch
+config['n_iter'] = 2 # For PatchMatch
+
+config['alphas'] = [0.1, 0.6, 0.7, 0.8, 1.] # For reconstruction blending
