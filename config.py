c7d12c2036044ec61f9f992db0b93fec14f314d4
julienroy13 <jul.roy1311@gmail.com>
2017-11-27 20:51:44 -0500

    Upsampling option and Optimizer option

--- config.py
+++ config.py
@@ -11,9 +11,11 @@ config['pool_mode'] = 'avg' # 'avg' or 'max'
 config['patch_size'] = 3 # For PatchMatch
 config['number_of_patches_per_zone'] = 1 # For randomSearch in PatchMatch
 config['n_iter'] = 3 # For PatchMatch
-config['distance_mode'] = 'unidirectional' # 'unidirectional' or 'bidirectional'
+config['distance_mode'] = 'bidirectional' # 'unidirectional' or 'bidirectional'
 
 # RECONSTRUCTION
-config['n_iter_deconv'] = 600 # For Reconstruction
+config['n_iter_deconv'] = 4500 # For Reconstruction
+config['optimizer'] = torch.optim.Adamax # torch.optim.Adamax, torch.optim.LBFGS
 config['loss_fct'] = torch.nn.L1Loss # torch.nn.L1Loss, torch.nn.MSELoss
 config['alphas'] = [0.1, 0.6, 0.7, 0.8, 1.] # For reconstruction blending
+config['upsampling_ON'] = {1:True, 2:False, 3:False, 4:False, 5:False}
