526a833f7150dff91b1a019f9444dbd033f28d01
julienroy13 <jul.roy1311@gmail.com>
2017-12-07 17:32:11 -0500

    Clean-up

--- config.py
+++ config.py
@@ -5,25 +5,25 @@ global config
 config = {}
 
 # GPU to run on
-config['device'] = 3 # 0,1,2 or 3
+config['device'] = 0 # 0,1,2 or 3
+
+# IMAGES
 config['image1_path'] = os.path.join('Images', "avatar.png")
-config['image2_path'] = os.path.join('Images', "chris.png")
+config['image2_path'] = os.path.join('Images', "joconde.png")
 
 # VGG
-config['img_size'] = 256  # 64, 128, 256, 512
+config['img_size'] = 224  # 56, 112, 224, 448
 config['pool_mode'] = 'avg' # 'avg' or 'max'
 
 # PATCH-MATCH
-config['patch_size'] = {1:5, 2:5, 3:3, 4:3, 5:3} # For PatchMatch
-config['number_of_random_patches'] = {1:3, 2:3, 3:4, 4:4, 5:25} # For randomSearch in PatchMatch
-config['n_iter'] = 4 # For PatchMatch
+config['patch_size'] = {1:5, 2:5, 3:3, 4:3, 5:3}
+config['n_iter'] = 2
 config['distance_mode'] = 'bidirectional' # 'unidirectional' or 'bidirectional'
-config['propagation_mode'] = 'NoNM' # 'NoNM'(Neighbor of Neighbor's Match) or 'NM'(Neighbor's Match)
 config['random_search_max_step'] = {1:4, 2:4, 3:6, 4:6, 5:14} 
 
 # RECONSTRUCTION
-config['n_iter_deconv'] = 3000 # For Reconstruction
+config['n_iter_deconv'] = 3000
 config['optimizer'] = torch.optim.Adamax # torch.optim.Adamax, torch.optim.LBFGS
 config['loss_fct'] = torch.nn.MSELoss # torch.nn.L1Loss, torch.nn.MSELoss
-config['alphas'] = {1:0.1, 2:0.6, 3:0.7, 4:0.8, 5:1.} # For reconstruction blending
+config['alphas'] = {1:0.1, 2:0.6, 3:0.7, 4:0.8}
 config['upsampling_ON'] = {1:True, 2:True, 3:True, 4:True, 5:False}
